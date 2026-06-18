e4c5e09170c6d47e863e6b490d395ebf03420532
rralphlawrence <56391822+rralphlawrence@users.noreply.github.com>
2026-06-18 19:28:44 +0800

    READme update (#9)
    * fix: unused assertion
    
    * Convert to Typescript
    
    * Comments
    
    * Yaml file
    
    * Remove .env from repo, add .env.example, rename utiility to utility, fix selectors and class naming
    
    * Fix typo: remove leading space in env fallback, restore local .env
    
    * Comments
    
    * Description for Read me
    
    * Update README.md
    
    ---------
    
    Co-authored-by: DESKTOP-7G2N8KM\raiji <raijinhubtv@gmail.com>

---
+++ playwright.config.js
@@ -0,0 +1,92 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+const { allure } = require('allure-playwright');
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter:[
+  ['html', {open: 'never'}],
+  ['list'],['allure-playwright'],
+
+  ] ,
+  outputDir: 'test-results',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  
+  
+
+  use: {
+   browserName: 'chromium',
+    headless: true,
+    trace: 'on-first-retry',
+    viewport: { width: 1920, height: 10880 },
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+
+
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'],
+        channel: 'chrome',
+        viewport: { width: 1920, height: 1080 },
+        launchOptions: {
+          args: ['--start-maximized',
+            '--disable-blink-features=AutomationControlled'
+          ],
+          
+       },
+    },
+  }
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
+
